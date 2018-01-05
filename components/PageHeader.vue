96b51c8c4730009e8a8100917314c8b523f70b77
watabean <watabean@gmail.com>
2018-01-05 14:35:18 +0900

    ハンバーガーメニューが消えていたので再度追加

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,22 +1,21 @@
 <template>
   <nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">わたなべ歯科</a>
-      </div>
-      <div id="navbar" class="navbar-collapse collapse">
-        <ul class="nav navbar-nav">
-          <li><nuxt-link to="/">TOP</nuxt-link></li>
-          <li><nuxt-link to="/about">当医院について</nuxt-link></li>
-          <li><nuxt-link to="/introduce">院長あいさつ</nuxt-link></li>
-          <li><nuxt-link to="/access">アクセス</nuxt-link></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#"><i class="fa fa-home fa-lg fa-fw" aria-hidden="true"></i>多治見市金岡町2-104-1</a></li>
-          <li><a href="tel:0572-25-3612"><i class="fa fa-phone-square fa-lg fa-fw" aria-hidden="true"></i>0572-25-3612</a></li>
-        </ul>
-      </div>
-      <!--/.nav-collapse -->
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#global-menu">
+      <span class="sr-only">メニュー</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">わたなべ歯科</a>
+    <div class="collapse navbar-collapse" id="global-menu">
+      <ul class="nav navbar-nav">
+        <li><nuxt-link to="/">TOP</nuxt-link></li>
+        <li><nuxt-link to="/about">当医院について</nuxt-link></li>
+        <li><nuxt-link to="/introduce">院長あいさつ</nuxt-link></li>
+        <li><nuxt-link to="/access">アクセス</nuxt-link></li>
+        <li><a href="#"><i class="fa fa-home fa-lg fa-fw" aria-hidden="true"></i>多治見市金岡町2-104-1</a></li>
+        <li><a href="tel:0572-25-3612"><i class="fa fa-phone-square fa-lg fa-fw" aria-hidden="true"></i>0572-25-3612</a></li>
+      </ul>
     </div>
   </nav>
 </template>
